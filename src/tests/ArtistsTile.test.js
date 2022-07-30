b8be9790678b670cfc322a9245085222963aceff
Olawale <olapetanwale@outlook.com>
2022-07-30 08:21:58 +0100

    Edit package.json file for routing

---
+++ src/tests/ArtistsTile.test.js
@@ -0,0 +1,18 @@
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/index';
+import DetailPage from '../components/DetailPage'
+
+it('Header is rendered', () => {
+  const detail = renderer
+    .create(
+      <Provider store={store}>
+        <BrowserRouter>
+          <DetailPage />
+        </BrowserRouter>
+      </Provider>,
+    )
+    .toJSON();
+  expect(detail).toMatchSnapshot();
+});

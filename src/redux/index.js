aa74284cf7ec350a3c0952c48cce3a14d2c568a6
Olawale <olapetanwale@outlook.com>
2022-12-09 11:10:39 +0100

    Remove Redux middlewares

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import chartsReducer from './ChartsSlice';
 
 const store = configureStore({
   reducer: {
     charts: chartsReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 export default store;

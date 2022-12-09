1aa26bc91b833a32f1836fb62536b03f84909f6e
Olawale <olapetanwale@outlook.com>
2022-12-09 11:03:26 +0100

    Edit for screen size

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,22 +9,27 @@ const Header = () => {
 
   return (
     <div className="header-main">
-      <div className="header">
-        <Menu />
-        <p>all time charts</p>
-        <div>
-          <BsMic className="mic" />
-          <BsSearch />
+      <div className="header-s">
+        <div className="header">
+          <Menu />
+          <p>All time charts</p>
+          <div style={{ visibility: 'hidden' }}>
+            <BsMic className="mic" />
+            <BsSearch />
+          </div>
         </div>
-      </div>
-      <div className="mid-head">
-        <div className="tracks">
-          <p>{charts.length}</p>
-          <p>Tracks</p>
-        </div>
-        <div className="deezer-logo">
-          <p className="deezer-text">Powered by</p>
-          <img style={{ width: '100px', marginRight: '24px' }} src={logo} alt="Deezer logo" />
+        <div className="mid-head">
+          <div className="tracks">
+            <p>
+              {charts.length}
+              {' '}
+              Tracks
+            </p>
+          </div>
+          <div className="deezer-logo">
+            <p className="deezer-text">Powered by</p>
+            <img style={{ width: '100px', marginRight: '24px' }} src={logo} alt="Deezer logo" />
+          </div>
         </div>
       </div>
 
